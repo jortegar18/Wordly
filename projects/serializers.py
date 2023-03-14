4fe7113145de6deb79e991af746e330752e75225
jortegar18 <72839162+jortegar18@users.noreply.github.com>
2023-03-13 20:37:12 -0500

    test

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Project
+
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
