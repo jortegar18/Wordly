5d07900c8a09c8aae4b98a9fbb43f2da0e163609
jortegar18 <72839162+jortegar18@users.noreply.github.com>
2023-05-28 19:57:55 -0500

    deploy

--- issimplecrud/settings.py
+++ issimplecrud/settings.py
@@ -176,7 +176,7 @@ DATETIME_INPUT_FORMATS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 if not DEBUG:    
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
