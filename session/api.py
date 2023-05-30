28cb6e118ffbd45c7be095a89519763b87dafb5c
jortegar18 <72839162+jortegar18@users.noreply.github.com>
2023-05-29 23:20:38 -0500

    time_availability

--- session/api.py
+++ session/api.py
@@ -22,6 +22,15 @@ def get_sessions_by_tutor(request):
         return Response(sessions_serializer.data, status = status.HTTP_200_OK)
     return Response({"message": "Primero debe iniciar sesion"}, status = status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+def get_sessions_by_student(request):
+
+    if request.user.is_authenticated:
+        sessions = Session.objects.filter(student = request.user.id)
+        sessions_serializer = SessionSerializer(sessions, many = True)
+        return Response(sessions_serializer.data, status = status.HTTP_200_OK)
+    return Response({"message": "Primero debe iniciar sesion"}, status = status.HTTP_400_BAD_REQUEST)
+
 @api_view(["GET"])
 def get_session_by_id(request, id):
 
--- session/urls.py
+++ session/urls.py
@@ -3,12 +3,13 @@ from session.api import *
 
 urlpatterns = [
     path('api/sessions/', get_sessions, name = "api_get_sessions"),
-    path('api/my-sessions/', get_sessions_by_tutor, name = "api_get_my_sessions"),
+    path('api/my-sessions/tutors/', get_sessions_by_tutor, name = "api_get_my_sessions"),
+    path('api/my-sessions/students/', get_sessions_by_student, name = "api_get_my_sessions"),
     path('api/sessions/create-session', insert_session, name = "api_insert_session"),
     path('api/sessions/session/<int:id>', get_session_by_id, name = "api_get_detail_session"),
     path('api/sessions/session/<int:id>/update', update_session, name = "api_update_session"),
     path('api/sessions/session/<int:id>/delete', delete_session, name = "api_delete_session"),
-    #path('api/sessions/session/<int:id>/register-volunteer', insert_volunteer_in_session, name = "api_register_volunteer"),
-    #path('api/sessions/session/get_requests', getVolunteerRequests, name="api_volunteer_requests"),
-    #path('api/sessions/session/<int:id>/create-turn', create_turn, name="api_create_turn"),
+    path('api/sessions/session/<int:id>/register-student', insert_student_in_session, name = "api_register_student"),
+    path('api/sessions/session/get_requests', getVolunteerRequests, name="api_volunteer_requests"),
+    path('api/sessions/session/<int:id>/create-turn', create_turn, name="api_create_turn"),
 ]
