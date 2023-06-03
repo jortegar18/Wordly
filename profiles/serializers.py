eaa0fb50c22862d0c192748b0d9050a28470ba78
jortegar18 <72839162+jortegar18@users.noreply.github.com>
2023-06-03 11:04:02 -0500

    mediafiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,12 +4,12 @@ from rest_framework import serializers
 class TutorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tutor
-        fields = ['username', 'email', 'name', 'last_name', 'gender', 'birthday', 'description', 'cost', 'calification', 'payment','expire_date','ccv']
+        fields = ['username', 'email', 'name', 'last_name', 'gender', 'profile_picture', 'birthday', 'description', 'cost', 'calification', 'payment','expire_date','ccv']
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Student
-        fields = ['username', 'email', 'name', 'last_name', 'gender', 'birthday', 'payment','expire_date','ccv']
+        fields = ['username', 'email', 'name', 'last_name', 'profile_picture', 'gender', 'birthday', 'payment','expire_date','ccv']
  
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
